fbadc3075539d8eb530268dbde4f0c939be44e2e
augustine mudrak <augustinemudrak@gmail.com>
2015-07-20 10:55:46 -0400

    working logic

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,7 +2,10 @@
 //  	$("playerOneButton").click(function(event){
 //  		$("<h1>").text("") = $("#playerOneInput").val()})
 // }
-var i = 0
+var i = 0;
+var gameArray= [[0,0,0],
+				[0,0,0],
+				[0,0,0]];
 
 console.log("linked?");
 
@@ -19,81 +22,120 @@ $("#playerTwoButton").click(function(){
  	})
 	//Counter for function
 
-	var counter= function counter(){
-		if (i%2 === 0) {
+	var counter= function counter(row, colum){
+		if (i%2 === 0) {	
 		i= i+1;
 		console.log("X");
+		gameArray[row][colum]= 1;
+		gameChecker();
 		return "url('http://i.imgur.com/n7U1bEYb.jpg')";
 		} else {
-			i= i+1;
-			console.log("Y");
-			return "url('http://i.imgur.com/e9Hb6DSb.jpg')";
+		  i= i+1;
+		  console.log("Y");
+		  gameArray[row][colum] = 2;
+		  gameChecker();
+		  return "url('http://i.imgur.com/e9Hb6DSb.jpg')";
 		}
 	}
 	//How to change a back ground image
 $("#0").click(function(){
 	console.log("hey man");
-	$('#0').css('background-image', counter());
+	$('#0').css('background-image', counter(0, 0));
 	$('#0').css("background-size" , "cover");
 })
 $("#1").click(function(){
 	console.log("something came in the mail today");
-	$('#1').css('background-image', counter());
-	$('#1').css('background-size' , 'cover');
+	$('#1').css('background-image', counter(0, 1));
+	$('#1').css('background-size' , 'cover'); 
 })
 $("#2").click(function(){
 	console.log("oh word?");
-	$('#2').css('background-image', counter());
+	$('#2').css('background-image', counter(0,2));
 	$('#2').css('background-size' , 'cover');
 })
 $("#3").click(function(){
 	console.log("what came in the mail?");
-	$('#3').css('background-image', counter());
+	$('#3').css('background-image', counter(1,0));
 	$('#3').css('background-size' , 'cover');
 })
 $("#4").click(function(){
 	console.log("......");
-	$('#4').css('background-image', counter());
+	$('#4').css('background-image', counter(1,1));
 	$('#4').css('background-size' , 'cover');
 })
 $("#5").click(function(){
 	console.log("..........");
-	$('#5').css('background-image', counter());
+	$('#5').css('background-image', counter(1,2));
 	$('#5').css('background-size' , 'cover');
 })
 $("#6").click(function(){
 	console.log("............");
-	$('#6').css('background-image', counter());
+	$('#6').css('background-image', counter(2,0));
 	$('#6').css('background-size' , 'cover');
 })
 $("#7").click(function(){
 	console.log("deezzz nuts");
-	$('#7').css('background-image', counter());
+	$('#7').css('background-image', counter(2,1));
 	$('#7').css('background-size' , 'cover');
 })
 $("#8").click(function(){
 	console.log("HA gottem");
-	$('#8').css('background-image', counter());
+	$('#8').css('background-image', counter(2,2));
 	$('#8').css('background-size' , 'cover');
 })
+///checks for winner
+var gameChecker = function gameChecker () {
+	if (gameArray[0][0] === 1 && gameArray[0][1] === 1 && gameArray [0][2] === 1) {
+		console.log("top row");
+	} else if (gameArray[1][0] === 1 && gameArray[1][1] === 1 && gameArray [1][2] === 1) {
+		console.log("middle row");
+	} else if (gameArray[2][0] === 1 && gameArray[2][1] === 1 && gameArray [2][2] === 1) {
+		console.log("bottom won");
+	} else if (gameArray[0][0] === 1 && gameArray[1][0] === 1 && gameArray [2][0] === 1) {
+		console.log("left row");
+	} else if (gameArray[0][1] === 1 && gameArray[1][1] === 1 && gameArray [2][1] === 1) {
+		console.log("middle row");
+	} else if (gameArray[0][2] === 1 && gameArray[1][2] === 1 && gameArray [2][2] === 1) {
+		console.log("right row");
+	} else if (gameArray[0][0] === 1 && gameArray[1][1] === 1 && gameArray [2][2] === 1) {
+		console.log("left to right diagonal");
+	} else if (gameArray[2][0] === 1 && gameArray[1][1] === 1 && gameArray [0][2] === 1) {
+		console.log("right to left diagonal");
+	} else if (gameArray[0][0] === 2 && gameArray[0][1] === 2 && gameArray [0][2] === 2) {
+		console.log("top row sung");
+	} else if (gameArray[1][0] === 2 && gameArray[1][1] === 2 && gameArray [1][2] === 2) {
+		console.log("middle row sung");
+	} else if (gameArray[2][0] === 2 && gameArray[2][1] === 2 && gameArray [2][2] === 2) {
+		console.log("bottom won sung");
+	} else if (gameArray[0][0] === 2 && gameArray[1][0] === 2 && gameArray [2][0] === 2) {
+		console.log("left row sung");
+	} else if (gameArray[0][1] === 2 && gameArray[1][1] === 2 && gameArray [2][1] === 2) {
+		console.log("middle row sung");
+	} else if (gameArray[0][2] === 2 && gameArray[1][2] === 2 && gameArray [2][2] === 2) {
+		console.log("right row sung");
+	} else if (gameArray[0][0] === 2 && gameArray[1][1] === 2 && gameArray [2][2] === 2) {
+		console.log("left to right diagonal sung");
+	} else if (gameArray[2][0] === 2 && gameArray[1][1] === 2 && gameArray [0][2] === 2) {
+		console.log("right to left diagonal sung");
+	}
+};
 
 
 
 
+// //the Modal
+// console.log("The monster mash - linked")
 
-//the Modal
-console.log("The monster mash - linked")
+// var myButton = $('#modal-button');
+// var datModal = $('#modal');
+// var closeButton = $('#close-modal')
 
-var myButton = $('#modal-button');
-var datModal = $('#modal');
-var closeButton = $('#close-modal')
+// myButton.on('click', function() {
+//   console.log("You clicked me oh lawd");
+//   datModal.toggle();
+//   console.log(datModal)
+// });
 
-myButton.on('click', function() {
-  console.log("You clicked me oh lawd");
-  datModal.toggle();
-  console.log(datModal)
-});
-
-closeButton.on('click', function() {
-  datModal.toggle();
-})
+// closeButton.on('click', function() {
+//   datModal.toggle();
+// });
